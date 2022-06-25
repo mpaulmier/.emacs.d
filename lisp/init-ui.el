67b857e42e081749f4a2a11168bc0917a51b2dc3
Matthias Paulmier <hello@mattplm.fr>
2022-06-25 12:52:06 +0200

    Update documentation
    That readme was weird, the config was never inspired from centaure emacs, I
    never used it, don't know what I meant 🤨
    
    - Update the readme
    - Update the copyright year everywhere

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,4 +1,4 @@
-;; Copyright (C) 2020  Matthias Paulmier
+;; Copyright (C) 2022  Matthias Paulmier
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
