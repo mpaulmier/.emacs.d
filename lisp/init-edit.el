7d6a999281fd17afb69f04eb4ed59c2025404475
Matthias Paulmier <hello@mattplm.fr>
2023-04-09 22:52:00 +0200

    Only enable move-text keys in prog-mode for now

--- lisp/init-edit.el
+++ lisp/init-edit.el
@@ -14,7 +14,8 @@
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (use-package move-text
-  :bind (("M-p" . move-text-up)
+  :bind (:map prog-mode-map
+         ("M-p" . move-text-up)
          ("M-n" . move-text-down)))
 
 (use-package subword
